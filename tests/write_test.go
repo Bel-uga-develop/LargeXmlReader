6768e9bdb12babdaee54b824a85b8377b89adaa6
Rafael Abuov <rafael.ab@mail.ru>
2023-04-29 19:13:10 +0600

    tests

--- tests/write_test.go
+++ tests/write_test.go
@@ -1,26 +1,26 @@
-package main
+package test
 
 import (
-	"fmt"
 	"strconv"
+	"testing"
 
 	"github.com/Bel-uga-develop/XmlReader"
 )
 
-func main() {
+func TestCreateFile(t *testing.T) {
 	writer := &XmlReader.Writer{}
-	if err := writer.Create("1.xml"); err != nil {
-		fmt.Println(err)
+	if err := writer.Create("data_2.xml"); err != nil {
+		t.Error(`error create file`)
 	}
 
 	for i := 0; i < 10; i++ {
 		data := "<element>\n" + strconv.Itoa(i) + "\n" + "</element>\n"
 		if err := writer.Write([]byte(data)); err != nil {
-			fmt.Println(err)
+			t.Error(`error write file`)
 		}
 	}
 
 	if err := writer.Close(); err != nil {
-		fmt.Println(err)
+		t.Error(`error close file`)
 	}
 }
