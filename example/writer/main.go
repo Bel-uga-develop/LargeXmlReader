cda81c520b3fc4141a5b4d91e217c6186777e6e3
Rafael Abuov <rafael.ab@mail.ru>
2023-05-20 15:27:24 +0600

    exmple writer debug

--- example/writer/main.go
+++ example/writer/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
-	"strconv"
 
 	"github.com/Bel-uga-develop/XmlReader"
 )
+
+type PublshInformation struct {
+	Publish     string `xml:"Publish"`
+	RecordCount int    `xml:"Record_Count"`
+}
 
 func main() {
 	writer := &XmlReader.Writer{}
@@ -13,11 +18,14 @@ func main() {
 		fmt.Println(err)
 	}
 
-	for i := 0; i < 10; i++ {
-		data := "<element>\n" + strconv.Itoa(i) + "\n" + "</element>\n"
-		if err := writer.Write([]byte(data)); err != nil {
-			fmt.Println(err)
-		}
+	data1 := &PublshInformation{
+		Publish:     "test",
+		RecordCount: 1,
+	}
+
+	data, _ := xml.MarshalIndent(data1, " ", " ")
+	if err := writer.Write([]byte(data)); err != nil {
+		fmt.Println(err)
 	}
 
 	if err := writer.Close(); err != nil {
